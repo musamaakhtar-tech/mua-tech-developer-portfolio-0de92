6fadfc19d6989fd420587c217b10626d406d2aef
M. Usama Akhtar <musamaakhtar63@gmail.com>
2026-05-07 01:37:42 +0500

    Refactor footer credits display
    Refactored footer component to streamline the display of credits.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,18 +10,16 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by M. Usama Akhtar")}
-        </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
-          <a
-            href="https://github.com/musamaakhtar-tech"
-            target="_blank"
-            rel="noreferrer"
-          >
-            MUA Tech
-          </a>
-        </p>
+  {emoji("Made with ❤️ by ")} (
+  <a
+    href="https://github.com/musamaakhtar-tech"
+    target="_blank"
+    rel="noreferrer"
+  >
+    MUA Tech
+  </a>
+  )
+</p>
       </div>
     </Fade>
   );
